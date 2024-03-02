5ea67b2ba688309961bf8e4e7a7949e52e65f1b3
Benjamin Tarver <benjamin.c.tarver@gmail.com>
2024-03-01 16:26:36 -0800

    Add "end" statement and indent enforceBCs code

--- functions/enforceBCs.m
+++ functions/enforceBCs.m
@@ -1,36 +1,37 @@
 function [u,v,p,T,rho] = enforceBCs(u,v,p,T,rho,pinf,Tinf,uinf,R)
-
-% Enforce BCs on primitive variables
-% Left boundary
-u(1,:) = uinf;
-v(1,:) = 0;
-p(1,:) = pinf;
-T(1,:) = Tinf;
-rho(1,:) = p(1,:)./(R.*T(1,:));
-
-% Right boundary
-u(end,:) = 2*u(end-1,:)-u(end-2,:);
-v(end,:) = 2*v(end-1,:)-v(end-2,:);
-p(end,:) = 2*p(end-1,:)-p(end-2,:);
-T(end,:) = 2*T(end-1,:)-T(end-2,:);
-rho(end,:) = p(end,:)./(R.*T(end,:));
-
-% Top boundary: overwrites top right corner point
-u(:,end) = uinf;
-v(:,end) = 0;
-p(:,end) = pinf;
-T(:,end) = Tinf;
-rho(:,end) = p(:,end)./(R.*T(:,end));
-
-% Bottom boundary: overwrites bottom right corner point
-u(:,1) = 0;
-v(:,1) = 0;
-p(:,1) = 2*p(:,2)-p(:,3);
-T(:,1) = Tinf;
-rho(:,1) = p(:,1)./(R.*T(:,1));
-
-% Bottom left corner point
-u(1,1) = 0;
-v(1,1) = 0;
-p(1,1) = pinf;
-T(1,1) = Tinf;
+    
+    % Enforce BCs on primitive variables
+    % Left boundary
+    u(1,:) = uinf;
+    v(1,:) = 0;
+    p(1,:) = pinf;
+    T(1,:) = Tinf;
+    rho(1,:) = p(1,:)./(R.*T(1,:));
+    
+    % Right boundary
+    u(end,:) = 2*u(end-1,:)-u(end-2,:);
+    v(end,:) = 2*v(end-1,:)-v(end-2,:);
+    p(end,:) = 2*p(end-1,:)-p(end-2,:);
+    T(end,:) = 2*T(end-1,:)-T(end-2,:);
+    rho(end,:) = p(end,:)./(R.*T(end,:));
+    
+    % Top boundary: overwrites top right corner point
+    u(:,end) = uinf;
+    v(:,end) = 0;
+    p(:,end) = pinf;
+    T(:,end) = Tinf;
+    rho(:,end) = p(:,end)./(R.*T(:,end));
+    
+    % Bottom boundary: overwrites bottom right corner point
+    u(:,1) = 0;
+    v(:,1) = 0;
+    p(:,1) = 2*p(:,2)-p(:,3);
+    T(:,1) = Tinf;
+    rho(:,1) = p(:,1)./(R.*T(:,1));
+    
+    % Bottom left corner point
+    u(1,1) = 0;
+    v(1,1) = 0;
+    p(1,1) = pinf;
+    T(1,1) = Tinf;
+end
