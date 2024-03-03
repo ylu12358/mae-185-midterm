f01a697762b5f8aec28336574ab8b5355c962a79
tarunkm02 <46302386+tarunkm02@users.noreply.github.com>
2024-03-02 17:02:24 -0800

    Implemented normalized plots

--- main.m
+++ main.m
@@ -34,8 +34,9 @@ figureskipped = 50; % only plots iterations at increments of this value
 t = 0;
 
 plotschlieren = false;
-plotadiabaticwall = false;
-bc = "isothermal";
+plotnormalized = true;
+
+bc = "adiabatic";
 
 %% initialize grid
 [xx,yy] = ndgrid(linspace(0,L,nx),linspace(0,H,ny));
@@ -78,7 +79,8 @@ U = prim2cons(rho,u,v,T,cv);
 f1 = figure(1);
 f1.Position = [100,100,1400,1000];
 
-varsplot = zeros(6,nx,ny);
+varsplot1 = zeros(6,nx,ny);
+varsplot2 = zeros(6,ny);
 
 cblabels = {'\rho [kg/m^3]',...
     'u [m/s]',...
@@ -94,33 +96,27 @@ subtitles = {'density',...
     'pressure',...
     'temperature'};
 
-if plotadiabaticwall == true
-    cblabels = {'\rho [kg/m^3]',...
-    'u [m/s]',...
-    'v [m/s]',...
-    'e [J/kg = m^3/s^3]',...
-    'p [Pa = kg/m/s^2]',...
-    'T [K]'};
-
-    subtitles = {'density',...
-    'velocity - x',...
-    'velocity - y',...
-    'spec. int. energy',...
-    'pressure',...
-    'temperature'};
+if plotnormalized == true
+    cblabels = {'T/T_{inf}',...
+        'T/T_{inf}',...
+        'T/T_{inf}',...
+        'p/p_{inf}',...
+        'p/p_{inf}',...
+        'p/p_{inf}'};
+    
+    subtitles = {'Temperature T @ x/L = 0.25',...
+        'Temperature T @ x/L = 0.5',...
+        'Temperature T @ x/L = 0.75',...
+        'Pressure p @ x/L = 0.25',...
+        'Pressure p @ x/L = 0.5',...
+        'Pressure p @ x/L = 0.75'};
+    
 end
 
 
 if plotschlieren == true
     cblabels{1} = ' ';
     subtitles{1} = 'schlieren';
-end
-
-if plotadiabaticwall == true
-    bc = "adiabatic";
-    cblabels{5,6} = ' ';
-    subtitles{5} = 'pressure; adiabatic wall';
-    subtitles{6} = 'temperature; adiabatic wall';
 end
 
 convergencevar = rho(40,20);
@@ -140,55 +136,86 @@ for iter = 1:nt
             
         % extract conservative variables for plot
         [rho,u,v,T,p,e,~] = cons2prim(U,R,cv);
-    
-        varsplot(1,:,:) = rho;
-        if plotschlieren == true
-            varsplot(1,:,:) = schlieren(rho,dx,dy);
-        end
-        varsplot(2,:,:) = u;
-        varsplot(3,:,:) = v;
-        varsplot(4,:,:) = e;
-        varsplot(5,:,:) = p;
-        varsplot(6,:,:) = T;
-        end
-    
-        % overall title
-        sgtitle(['MacCormack for Compressible Navier-Stokes: t = ' num2str(t) ', n = ' num2str(iter)]);
-    
-        % pcolor
-        for j = 1:size(varsplot,1)
-            ax = subplot(3,3,j);
-            pcolor(xx,yy,squeeze(varsplot(j,:,:)));
-            shading interp;
-            cb(j) = colorbar;
-            ylabel(cb(j),cblabels(j));
-            if j == 1 && plotschlieren == true % schlieren
-                colormap(ax,"gray");
-                clim([0 1]);
-            else
-                colormap(ax,"turbo");
-            end
-            axis equal tight;
-            title(subtitles(j));
-            xlabel('x'); ylabel('y');
-            set(gca,'FontSize',12);
-        end
+        
+        if plotnormalized == false
 
-        % convergence
-        convergencevar = [convergencevar rho(40,20)];
-        convergencet = [convergencet  t];
-        subplot(3,3,[7 8 9]);
-        plot(convergencet,convergencevar,'r-','LineWidth',2);
-        hold on; box on; grid on;
-        xlim([0 t]);
-        title(['convergence of density at x = ' num2str(40*dx) ...
-            ', y = ' num2str(20*dy)]);
-        xlabel('t'); ylabel('\rho');
-        set(gca,'FontSize',14);
+            varsplot1(1,:,:) = rho;
+            if plotschlieren == true
+                varsplot1(1,:,:) = schlieren(rho,dx,dy);
+            end
+            varsplot1(2,:,:) = u;
+            varsplot1(3,:,:) = v;
+            varsplot1(4,:,:) = e;
+            varsplot1(5,:,:) = p;
+            varsplot1(6,:,:) = T;
+            
+        
+            % overall title
+            sgtitle(['MacCormack for Compressible Navier-Stokes: t = ' num2str(t) ', n = ' num2str(iter)]);
+       
+            % pcolor
+            for j = 1:size(varsplot1,1)
+                ax = subplot(3,3,j);
+                pcolor(xx,yy,squeeze(varsplot1(j,:,:)));
+                shading interp;
+                cb(j) = colorbar;
+                ylabel(cb(j),cblabels(j));
+                if j == 1 && plotschlieren == true % schlieren
+                    colormap(ax,"gray");
+                    clim([0 1]);
+                else
+                    colormap(ax,"turbo");
+                end
+                axis equal tight;
+                title(subtitles(j));
+                xlabel('x'); ylabel('y');
+                set(gca,'FontSize',12);
+            end
+        
+            % convergence
+            convergencevar = [convergencevar rho(40,20)];
+            convergencet = [convergencet  t];
+            subplot(3,3,[7 8 9]);
+            plot(convergencet,convergencevar,'r-','LineWidth',2);
+            hold on; box on; grid on;
+            xlim([0 t]);
+            title(['convergence of density at x = ' num2str(40*dx) ...
+                ', y = ' num2str(20*dy)]);
+            xlabel('t'); ylabel('\rho');
+            set(gca,'FontSize',14);
+       
+        else
+            
+            varsplot2(1,:) = T(19,:)./Tinf;
+            varsplot2(2,:) = T(38,:)./Tinf;
+            varsplot2(3,:) = T(56,:)./Tinf;
+            varsplot2(4,:) = p(19,:)./pinf;
+            varsplot2(5,:) = p(38,:)./pinf;
+            varsplot2(6,:) = p(56,:)./pinf;
+            
+            % overall title
+            if bc == "isothermal"
+                sgtitle(['Normalized NSE w/ Isothermal BC: t = ' num2str(t) ', n = ' num2str(iter)]);
+            elseif bc == "adiabatic"
+                sgtitle(['Normalized NSE w/ Adiabatic BC: t = ' num2str(t) ', n = ' num2str(iter)]);
+            end
+
+            % pcolor
+            for j = 1:size(varsplot2,1)
+                ax = subplot(2,3,j);
+                plot(yy(1,:),varsplot2(j,:));
+                title(subtitles(j));
+                xlabel('y'); ylabel(cblabels(j));
+                set(gca,'FontSize',12);
+            end
+            
+        end 
+        
 
         drawnow;
     end
 end
+
 
 %% end timer
 toc
